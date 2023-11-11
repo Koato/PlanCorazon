d48bccc9fd937019792efe2f57dc6fa076433f36
Andy Gomez <andy.gomez@softtek.com>
2023-11-10 22:38:08 -0500

    create project

---
+++ src/main/java/com/encora/ibk/plancorazon/PlancorazonApplication.java
@@ -0,0 +1,13 @@
+package com.encora.ibk.plancorazon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlancorazonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlancorazonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/encora/ibk/plancorazon/PlancorazonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encora.ibk.plancorazon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlancorazonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
